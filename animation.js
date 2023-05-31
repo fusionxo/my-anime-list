65005e3724b24bec8b6d11d047d69f965ed2febf
fusionxo <107797673+fusionxo@users.noreply.github.com>
2023-06-01 01:44:30 +0530

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,141 @@
+const animeList = [
+  "Chainsaw Man",
+  "More than a married couple, but not lovers.",
+  "Tomodachi Game",
+  "Scum's Wish",
+  "Rent-a-Girlfriend 2nd Season",
+  "High School DxD New",
+  "Jujutsu Kaisen 0",
+  "Aharen-san wa Hakarenai",
+  "Vermeil in Gold",
+  "High School DxD",
+  "Classroom of the Elite",
+  "Classroom of the Elite II",
+  "Harem in the Labyrinth of Another World (Uncensored)",
+  "My Stepmom's Daughter Is My Ex",
+  "TONIKAWA: Over The Moon For You",
+  "Into the Forest of Fireflies' Light",
+  "5 Centimeters Per Second",
+  "I Want To Eat Your Pancreas",
+  "A Silent Voice",
+  "Your Name.",
+  "Weathering with You",
+  "A Whisker Away",
+  "DARLING in the FRANXX",
+  "Komi Can't Communicate Season 2",
+  "Komi Can't Communicate",
+  "Horimiya",
+  "Rascal Does Not Dream of Bunny Girl Senpai",
+  "Attack on Titan: The Final Season Part 2",
+  "Attack on Titan: The Final Season",
+  "Attack on Titan Season 3 Part 2",
+  "Attack on Titan Season 3",
+  "Attack on Titan Season 2",
+  "Attack on Titan",
+  "Jujutsu Kaisen",
+  "Hyouka",
+  "Demon Slayer: Kimetsu no Yaiba Swordsmith Village Arc",
+  "Loving Yamada at Lv999",
+  "Tonikawa: Over The Moon For You: 2nd Season",
+  "Hell's Paradise",
+  "The Legendary Hero Is Dead!",
+  "I Got a Cheat Skill in Another World and Became Unrivaled in The Real World, Too",
+  "The Devil is a Part-Timer!",
+  "Wise Man's Grandchild",
+  "Summer Ghost",
+  "The Disastrous Life of Saiki K. 2",
+  "The Disastrous Life of Saiki K.",
+  "Re:Zero - Starting Life in Another World 2nd Season (Part 2)",
+  "Re:Zero - Starting Life in Another World",
+  "Re:Zero - Starting Life in Another World 2nd Season",
+  "Teasing Master Takagi-san",
+  "Tokyo Revengers",
+  "Tokyo Revengers: Christmas Showdown",
+  "Tokyo Ghoul",
+  "The Angel Next Door Spoils Me Rotten",
+  "Don't Toy with Me, Miss Nagatoro",
+  "Tomo-chan Is a Girl!",
+  "ReLIFE: Kanketsu-hen",
+  "ReLIFE",
+  "The Daily Life of the Immortal King 3",
+  "The Daily Life of the Immortal King 2",
+  "The Daily Life of the Immortal King",
+  "The Quintessential Quintuplets: The Movie",
+  "Masamune-kun's Revenge",
+  "Ao Haru Ride OVA",
+  "Blue Spring Ride (Ao Haru Ride)",
+  "The Quintessential Quintuplets",
+  "Beast Tamer",
+  "The Garden of Words",
+  "Death Note",
+  "Uzaki-chan Wants to Hang Out!",
+  "Uzaki-chan wa Asobitai! 2nd Season",
+  "Spy x Family",
+  "Spy x Family Part 2",
+  "My Dress-Up Darling",
+  "Love, Chunibyo & Other Delusions!",
+  "Love, Chunibyo & Other Delusions! 2nd Season: Heart Throb",
+  "The Eminence in Shadow",
+  "Call of the Night",
+  "Death March to the Parallel World Rhapsody",
+  "The Misfit of Demon King Academy",
+  "Didn't I Say to Make My Abilities Average in the Next Life?!"
+];
+
+
+
+const animeListContainer = document.getElementById("anime-list");
+
+animeList.forEach((anime, index) => {
+  const listItem = document.createElement("li");
+  const number = document.createElement("span");
+  number.classList.add("number");
+  number.textContent = `${(index + 1).toString().padStart(2, "0")} `;
+  listItem.appendChild(number);
+
+  const animeTitle = document.createElement("span");
+  animeTitle.textContent = anime;
+  listItem.appendChild(animeTitle);
+
+  const animeInfo = document.createElement("span");
+  animeInfo.classList.add("anime-info");
+  animeInfo.textContent = `Information about ${anime}`;
+  listItem.appendChild(animeInfo);
+
+  animeTitle.addEventListener("click", function() {
+    animeInfo.classList.toggle("show-info");
+  });
+
+  animeInfo.addEventListener("click", function() {
+    const searchQuery = anime.replace(/\s/g, "+");
+    const searchURL = `https://myanimelist.net/search/all?q=${searchQuery}`;
+    window.open(searchURL, "_blank");
+  });
+
+  animeListContainer.appendChild(listItem);
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const dot = document.createElement("div");
+  dot.classList.add("cursor-dot");
+  document.body.appendChild(dot);
+
+  document.addEventListener("mousemove", function(event) {
+    dot.style.left = event.clientX + "px";
+    dot.style.top = event.clientY + "px";
+
+    const animeTitles = document.querySelectorAll("li");
+    animeTitles.forEach(title => {
+      title.addEventListener("mouseenter", function() {
+        dot.classList.add("cursor-dot-hover"); // Add a class to change the dot color
+      });
+      title.addEventListener("mouseleave", function() {
+        dot.classList.remove("cursor-dot-hover"); // Remove the class to revert to original color
+      });
+    });
+  });
+});
+
